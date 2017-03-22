afa9c91fc61f60240cc81c15e6b6cea5bb328fae
Dieter Scholz <Dieter.Scholz@rudolf.de>
2017-03-22 15:13:54 +0100

    More basic stuff

--- csharp-learn/csharp-learn/Program.cs
+++ csharp-learn/csharp-learn/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C2B.CSharp.Learn
 {
@@ -25,7 +21,13 @@ namespace C2B.CSharp.Learn
         // struct -> value type
         public struct Point
         {
-            public int x, y;
+            public int X, Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
         }
 
         public BasicLanguage()
@@ -53,6 +55,73 @@ namespace C2B.CSharp.Learn
             int xInt = 1;
             short yShort = (short)xInt;
             Console.WriteLine(xInt.GetType() + " " + yShort.GetType());
+
+            // using a struct
+            // Point p = null; does not work - is a value type
+            Point p = new Point(1, 2);
+            Console.WriteLine(p.X + " " + p.Y);
+
+            // silently overflow
+            // checked would throw exeception
+            unchecked
+            {
+                int overflowInt;
+                overflowInt = int.MaxValue + 1;
+                Console.WriteLine(overflowInt);
+            }
+
+            // special decimal values
+            Console.WriteLine(1.0 / 0.0); // INFINITY
+            Console.WriteLine(0.0 / 0.0); // NaN
+
+            // bool type
+            bool isTrue = false;
+            Console.WriteLine(isTrue);
+
+            // char type
+            // \u unicode
+            char aChar = 'C';
+            Console.WriteLine(aChar);
+
+            // string - reference type
+            string aString = "Hello world";
+            Console.WriteLine(aString);
+
+            // verbatim string
+            string anotherString = @"c:\
+            test";
+            Console.WriteLine(anotherString);
+
+            // interpolated strings
+            string iString = $"{byte.MaxValue:X2}";
+            Console.WriteLine(iString);
+
+            // arrays
+            char[] vowels = new char[5];
+            vowels[0] = '1';
+            vowels[4] = '1';
+            foreach (char c in vowels)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+
+            // multidimensional
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = i * 3 + j;
+
+            // jagged
+            int[][] matrix2 = new int[][]
+            {
+                new int[] {0,1,2},
+                new int[] {3,4,5},
+                new int[] {6,7,8,9}
+            };
+
+            var doubleValue = 1.0;
+            Console.WriteLine(doubleValue);
         }
     }
 
